484350446f5cd6925508535cb9951e2fbac72e26
JeongHyun Seong <tjdwjdgus99@naver.com>
2023-09-18 13:00:21 +0900

    Add files via upload

---
+++ JAVA01_AndroidStudio/002.AndroidApp02/app/src/main/java/com/example/androidapp02/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.androidapp02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHello;
+    private Button btnOk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtHello = (TextView)findViewById(R.id.txtHello);
+        btnOk = (Button)findViewById(R.id.btnOk);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtHello.setText("안녕하세요. 안드로이드!!");
+            }
+        });
+    }
+
+
+
+}
+
+
