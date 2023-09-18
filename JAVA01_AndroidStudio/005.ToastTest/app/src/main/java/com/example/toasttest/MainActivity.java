cdc5b4071c696a5a28155ed3770cfa6e8c1cccd1
JeongHyun Seong <tjdwjdgus99@naver.com>
2023-09-18 13:03:00 +0900

    Add files via upload

---
+++ JAVA01_AndroidStudio/005.ToastTest/app/src/main/java/com/example/toasttest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.toasttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"버튼이 눌러졋습니다",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
